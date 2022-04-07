b8280db904a00c96e12a21e828bea32f6333d3b2
sanyas-dada <sanyasdada09@gmail.com>
2022-04-07 05:50:58 +0545

    guideline in constraint layout

--- app/src/main/java/com/sanyasdada/githubtutorial/MainActivity.java
+++ app/src/main/java/com/sanyasdada/githubtutorial/MainActivity.java
@@ -3,12 +3,23 @@ package com.sanyasdada.githubtutorial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.image_1);
+
+        System.out.println("i ma going tto change my logical thinking very ver fast rapidly");
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
